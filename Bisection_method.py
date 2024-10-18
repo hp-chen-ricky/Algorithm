f431f6d6229cdaf133486074b1fa4d1c223c87c8
ricky <ricky1999880205@gmail.com>
2024-10-18 12:01:35 +0800

    寫二分法

--- Bisection_method.py
+++ Bisection_method.py
@@ -1,6 +1,55 @@
 
+print("-----------------------------------")
+print("-----------------------------------")
 print("This is Bisection method")
-for i in range(10):
-    print(i)
-    
+print("-----------------------------------")
 
+def function_value(x):
+    #輸入方程式
+    value=x**3-5*x**2+10*x-80
+    #回傳函數值
+    return value
+#test 輸入2得出函數值
+value=function_value(2)
+# print(value)
+
+#輸入範圍[LB,UB]
+LB,UB=0,100
+
+#收斂到多小
+epsilon=1e-6
+
+def Bisection_method(LB,UB,epsilon):
+    #範圍中點 mp
+    mp=(LB+UB)/2
+
+    #求出函數值
+    value_LB=function_value(LB)
+    value_UB=function_value(UB)
+    value_mp=function_value(mp)
+    iteration=0
+    #當中點函數值很靠近收斂值 和 疊代小於100次
+    while abs(value_mp)>epsilon and iteration<100:
+        iteration=iteration+1
+        # print("iteration=",iteration)
+        mp=(LB+UB)/2
+        # print(LB,mp,UB)
+        value_LB=function_value(LB)
+        value_UB=function_value(UB)
+        value_mp=function_value(mp)
+        if value_UB*value_mp<0:
+            LB=mp
+        elif value_LB*value_mp<0:
+            UB=mp
+        # print("update ",LB,mp,UB)
+        # print("-----------------------------------")
+    root=(LB+UB)/2
+    return iteration,root
+
+
+#test
+iteration,root=Bisection_method(LB,UB,epsilon)
+print("iteration=",iteration)
+print("root= %.8f" % root)
+print("-----------------------------------")
+print("-----------------------------------")
